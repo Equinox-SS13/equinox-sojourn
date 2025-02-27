586a8d44db1fb7e8d415592515c64e1438e8a988
WeNeedMorePhoron <132836028+WeNeedMorePhoron@users.noreply.github.com>
2025-02-27 07:19:02 -0600

    Fix lint

--- tgui/packages/tgui/interfaces/OreBox.tsx
+++ tgui/packages/tgui/interfaces/OreBox.tsx
@@ -20,10 +20,10 @@ export const OreBox = props => {
   const { materials } = data;
 
   materials.sort((a, b) => {
-    if (a.name < b.name) { return -1 }
-    if (a.name > b.name) { return 1 }
+    if (a.name < b.name) { return -1; }
+    if (a.name > b.name) { return 1; }
     return 0;
-  })
+  });
 
   return (
     <Window width={600} height={265}>
